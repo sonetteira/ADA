7db4a0709b6081c254616bea4a90f04e71c18fbf
lk17362p@pace.edu <sonetteira@users.noreply.github.com>
2018-11-08 09:55:54 -0500

    pretty works

--- pretty.php
+++ pretty.php
@@ -5,7 +5,7 @@ include('functions.php');
 include('error_values.php');
 $conn = OpenCon();
 $yaxis = "timestamp";
-$xaxis = ["temp","ph","phmv","cond","dopct","domgl","dogain","turb","depth"];
+$xaxis = array_keys($sensor_list);
 #$sql = "SELECT ". $column_headers[$xaxis] . ", " . $column_headers[$yaxis] . " FROM ada_data LIMIT 672";
 $sql = "SELECT * FROM ada_data LIMIT 672";
 $result = $conn->query($sql);
@@ -21,19 +21,23 @@ if ($result->num_rows > 0) {
         }
         $i++;
     }
-    $new_data = comparable_axis(clean_data($data, $checks), ['timestamp',"temp","ph","phmv","cond","dopct","domgl","dogain","turb","depth"], 100); #remove bad data values
+    $new_data = comparable_axis(clean_data($data, $checks), array_merge([$yaxis],$xaxis), 100); #remove bad data values
     foreach($new_data as $row) { #create a table of x and y coordinates to graph
         //$t = convert_time($row[$yaxis]);
         $t = '"' . $row[$yaxis] . '"';
         $x[] = $t;
+        $good = [];
         foreach($xaxis as $s) {
             if(!is_nan($row[$s])) {  
                 $y[$s][] = $row[$s];
+                $good[] = $s;
             }
+            
         }
     }
 }
 CloseCon($conn);
+$k = 0;
 ?>
 <!-- Plotly.js -->
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
@@ -42,31 +46,27 @@ CloseCon($conn);
 <!-- Plotly chart will be drawn inside this DIV -->
 <div id="myDiv" style="width:100%;height:100%"></div>
 <script>
+sensors = ['<?php echo implode("','",$good); ?>'];
 x = [<?php echo implode(",",$x); ?>];
-y = [];
-y[0] = [<?php echo implode(",",$y['temp']); ?>];
-y[1] = [<?php echo implode(",",$y['ph']); ?>];
-y[2] = [<?php echo implode(",",$y['cond']); ?>];
-y[3] = [<?php echo implode(",",$y['depth']); ?>];
+y = [<?php foreach($y as $temp) {
+    echo '[', implode(",",$temp), '],'; }?>];
 incr = 2;
 max = x.length-1;
 i = max-incr;
-x1 = x.slice(i, max-1);
-y1 = [];
-y1[0] = y[0].slice(i, max-1);
-y1[1] = y[1].slice(i, max-1);
-y1[2] = y[2].slice(i, max-1);
-y1[3] = y[3].slice(i, max-1);
 data = [];
 for(j=0;j<y.length;j++) {
+    data[j] = {x:x.slice(i, max-1),
+        y:y[j].slice(i, max-1),
+        name: sensors[j]};
+}
+/*for(j=0;j<y.length;j++) {
     data[j] = {x:x,
         y:y[j]};
-}
+}*/
 Plotly.newPlot('myDiv', data, {
     xaxis: {range: ["2018-09-12 11:45:00", "2018-09-19 12:15:00"]},
     yaxis: {range: [0, 100]}
 });
-/*console.log(data);
 function next() {
     if(i-incr>0)
         i -= incr;
@@ -77,11 +77,8 @@ function next() {
         data[j] = {x:x.slice(i, max-1),
             y:y[j].slice(i, max-1)};
     }
-    console.log(data);
-    //y1 = y.slice(i, max-1);
     Plotly.animate('myDiv', {
         data: data,
-        traces: [0],
         layout: {}
     }, {
         transition: {
@@ -95,6 +92,6 @@ function next() {
     if(i>0)
         requestAnimationFrame(next);
 }
-requestAnimationFrame(next);*/
+requestAnimationFrame(next);
 </script>
 </body>
